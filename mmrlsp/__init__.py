c5c968747977e141fdbedec85be009e92a2db322
Álvaro Mondéjar <mondejar1994@gmail.com>
2022-03-04 17:46:13 +0100

    Add JSON schema validator (#9)

--- mmrlsp/__init__.py
+++ mmrlsp/__init__.py
@@ -8,20 +8,13 @@ import mkdocs
 from mkdocs.config.config_options import Type
 
 
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 
 
 class MkdocsMaterialRelativeLanguageSelectorPlugin(mkdocs.plugins.BasePlugin):
     config_scheme = (
-        ('github_pages', Type(bool, default=True)),
         ('root_domain', Type(bool, default=False)),
     )
-
-    def on_config(self, config, **kwargs):
-        if self.config['root_domain']:
-            self.config['github_pages'] = False
-        elif not self.config['github_pages']:
-            self.config['github_pages'] = True
 
     def on_files(self, files, config):
         """Add 'relative-material-language-selector.js' script to the files
